4f6486b7510d6744d63e4d2fdfe4636f1dd9c020
anchoras <anchoras2@gmail.com>
2021-02-01 18:11:42 +0300

    added demo for For_Each operator in multydementional arrays

--- ForEachDemo.java
+++ ForEachDemo.java
@@ -2,7 +2,7 @@
     demo program inludes intoduction For-Each cycle basics
 */
 
-class ForEachDemo {
+/* class ForEachDemo {
     public static void main (String args[]) {
         int  nums[] = {1, 2, 3, 4, 5, 6};
         
@@ -18,5 +18,27 @@ class ForEachDemo {
         for (int x : nums) {
             System.out.println("Value of element: " + x);
         }
+    }
+} */
+
+class ForEachDemo {
+    public static void main (String args[]) {
+        int sum = 0;
+        int nums[][] = new int[3][5];
+
+        for (int i=0; i<3; i++) {
+            for (int j=0; j<5; j++) {
+                nums[i][j] = (i+1) * (j+1);
+            }
+        }
+
+        for (int x[] : nums) {
+            for (int y : x) {
+                System.out.println("Value is: " + y);
+                sum += y;
+            }
+        }
+
+        System.out.println("Sum is: " + sum);
     }
 }
