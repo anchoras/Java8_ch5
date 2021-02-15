70a6a962614830490f2bc21a1741bd01e37eeb03
anchoras <anchoras2@gmail.com>
2021-02-15 12:52:03 +0300

    added demo for showing bites after using NOT operator

--- ShowBites.java
+++ ShowBites.java
@@ -16,5 +16,17 @@ class ShowBites {
             }
         }
         System.out.println();
+
+
+        byte notNumber = (byte) ~number;
+        for (t=128; t>0; t=t/2) {
+            if ((notNumber & t) == t) {
+                System.out.print("1");
+            }
+            else {
+                System.out.print("0");
+            }
+        }
+        System.out.println();
     }
 }
