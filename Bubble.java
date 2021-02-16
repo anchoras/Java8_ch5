61a913c2af91bc0908744252a38cd1e6818d192a
anchoras <anchoras2@gmail.com>
2021-02-16 12:26:10 +0300

    modified program for sorting String objects from command line arguments

--- Bubble.java
+++ Bubble.java
@@ -5,16 +5,19 @@
 class Bubble {
     public static void main(String args[]) 
         throws java.io.IOException {
-        int i, j, intTemp, size = 6, intInput;
+        int i, j, size, intInput;
         char ignore;
-        int myIntArray[] = new int[size];
+        
+        size = args.length;
+        String[] myStringArray = new String[size];
+        String stringTemp = new String();
 
-        myIntArray[0] = -13;
+/*         myIntArray[0] = -13;
         myIntArray[1] = 3;
         myIntArray[2] = 32;
         myIntArray[3] = 0;
         myIntArray[4] = 0;
-        myIntArray[5] = 17;
+        myIntArray[5] = 17; */
 /*         for (i=0; i<=(size-1); i++) {
             intInput = System.in.read();
             do {
@@ -25,22 +28,23 @@ class Bubble {
 
         System.out.println("unsorted array: ");
         for (i=0; i<=(size-1); i++) {
-            System.out.print(myIntArray[i] + " ");
+            myStringArray[i] = args[i];
+            System.out.print(myStringArray[i] + " ");
         }
 
         for (i=0; i<=(size-1); i++) {
             for (j=(size-1); j>i; j--) {
-                if (myIntArray[j-1] > myIntArray[j]) {
-                    intTemp = myIntArray[j-1];
-                    myIntArray[j-1] = myIntArray[j];
-                    myIntArray[j] = intTemp;
+                if (myStringArray[j].compareTo(myStringArray[j-1]) < 0) {
+                    stringTemp = myStringArray[j];
+                    myStringArray[j] = myStringArray[j-1];
+                    myStringArray[j-1] = stringTemp;
                 }
             }
         }
 
-        System.out.println("sorted array: ");
+        System.out.println("\nsorted array: ");
         for (i=0; i<=(size-1); i++) {
-            System.out.print(myIntArray[i] + " ");
+            System.out.print(myStringArray[i] + " ");
         }
     }
 }
